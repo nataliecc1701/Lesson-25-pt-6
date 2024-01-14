a5f53bbf5ccaa4f0b7502551a978dfbcab65d7e1
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-01-13 21:22:18 -0800

    seed for convenience

---
+++ seed.py
@@ -0,0 +1,23 @@
+from app import app
+from models import db, User, Feedback
+
+app.app_context().push()
+
+db.drop_all()
+db.create_all()
+
+users = [
+    User.register("StevieChicks", "ChickensRc00l", "steviechicks@springboard.com", "Stevie", "Chicks"),
+    User.register("NinthSaint", "IhttTss4ever", "harrowharkthefirst@mithraeum.gov", "Harrowhark", "Nonagesimus"),
+]
+
+db.session.add_all(users)
+db.session.commit()
+
+feedback = [
+    Feedback(title="I have always lived", content="I have never been killed and do not intend to start now", username="NinthSaint"),
+    Feedback(title="Bock Bock", content="cluck!", username="StevieChicks"),
+]
+
+db.session.add_all(feedback)
+db.session.commit()
