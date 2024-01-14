1fb539626cf9d904a3c4d29092370b76d8a032e2
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-01-13 19:37:51 -0800

    added user model

--- models.py
+++ models.py
@@ -10,4 +10,34 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
-# Models follow:
+# Models follow
+class User(db.Model):
+    __tablename__ = "users"
+    username = db.Column(db.String(20), primary_key = True, autoincrement = True)
+    password = db.Column(db.Text, nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique = True)
+    first_name = db.Column(db.String(30), nullable = False)
+    last_name = db.Column(db.String(30), nullable = False)
+    
+    
+    @classmethod
+    def register(cls, username, pwd):
+        """Register a user and store a salted hash of their password"""
+        
+        hashed = bcrypt.generate_password_hash(pwd)
+        hashed_utf8 = hashed.decode("utf8")
+        
+        return cls(username=username, password=hashed_utf8)
+    
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Authenticate a user by username and password
+        
+        Returns the user if valid, else returns False"""
+        
+        u = cls.query.filter_by(username=username).first()
+        
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            return u
+        else:
+            return False
