19d88b9af57990a869298e6e452fe5bcb1dc5caf
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-01-13 19:28:56 -0800

    set up venv imports

---
+++ app.py
@@ -0,0 +1,9 @@
+
+# Library imports
+from flask import Flask, render_template, redirect, session, flash
+from sqlalchemy.exc import IntegrityError
+
+# Local imports
+from models import connect_db, db
+# from forms import 
+from config import configure_app
